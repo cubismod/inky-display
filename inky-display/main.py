a34d91195da1ca9ab803891a64b0b60a267eddfd
Ryan <git@hexa.mozmail.com>
2024-11-09 16:58:59 -0500

    parsing

--- inky-display/main.py
+++ inky-display/main.py
@@ -2,6 +2,18 @@ import logging
 from inky import YELLOW
 from inky.auto import auto
 from PIL import Image, ImageDraw
+from os import environ
+from datetime import datetime, timedelta
+from redis.backoff import ExponentialBackoff
+from sortedcontainers import SortedDict
+from redis.retry import Retry
+from schedule_event import ScheduleEvent
+from redis.client import Redis
+from redis.exceptions import (
+   BusyLoadingError,
+   ConnectionError,
+   TimeoutError
+)
 
 logging.basicConfig(
     format="%(levelname)-8s %(message)s"
@@ -9,11 +21,46 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
+
+def get_redis_items(redis: Redis):
+   til = str((datetime.now() + timedelta.min(60)).timestamp())
+
+   items = redis.zrange('time', str(datetime.now().timestamp()), til, byscore=True, withscores=False)
+
+   pipeline = redis.pipeline()
+   [pipeline.get(item) for item in items]
+
+   return pipeline.execute(raise_on_error=False)
+
+
+
 def __main__():
     display = auto()
 
-    with Image.open("./display.png") as img:
-        display.set_image(img)
-        display.show()
+    retry = Retry(ExponentialBackoff(), 3)
+    r = Redis(
+        host=environ.get("REDIS_HOST"),
+        port=environ.get("REDIS_PORT"),
+        password=environ.get("REDIS_PASS"),
+        retry=retry,
+        retry_on_timeout=True,
+        retry_on_error=[BusyLoadingError, ConnectionError, TimeoutError]
+    )
+
+    departures = SortedDict[str, ScheduleEvent]
+    
+    while True:
+        json_events = get_redis_items(r)
+        for event in json_events:
+            if event:
+                schedule_event = ScheduleEvent.model_validate_json(event, strict=False)
+                # factor in travel time to the departure
+                departure = 
+                departures[str(schedule_event.time.timestamp())] = schedule_event
+
+
+
+        
+
 
 __main__()
---
+++ inky-display/schedule_event.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class ScheduleEvent(BaseModel):
+    action: str
+    time: datetime
+    route_id: str
+    route_type: int
+    headsign: str
+    stop: str
+    id: str
+    transit_time_min: int
